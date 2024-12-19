399a3e4e6c2a7081a6dfaac68b4142d4850fb573
H3Airness <damienraunier@gmail.com>
2024-12-19 22:29:25 +0100

    correction du dump sql

--- database/init.sql
+++ database/init.sql
@@ -5,10 +5,10 @@ COLLATE utf8mb4_unicode_ci;
 USE `blogdb`;
 
 CREATE USER IF NOT EXISTS 'myuser'@'%' IDENTIFIED BY 'admin1234';
-GRANT ALL PRIVILEGES ON blogdb.* TO 'myuser'@'%';
+GRANT ALL PRIVILEGES ON `blogdb`.* TO 'myuser'@'%';
 FLUSH PRIVILEGES;
 
-CREATE TABLE blog (
+CREATE TABLE IF NOT EXISTS `blog` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titre VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     description TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
@@ -17,10 +17,10 @@ CREATE TABLE blog (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO blog (titre, description, image_url, pseudo) VALUES 
+INSERT INTO `blog` (titre, description, image_url, pseudo) VALUES 
 (
     'Les merveilles des Alpes',
-    'Lorem ipsum dolor sit amet, consectetur  adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     'https://picsum.photos/seed/alpes/400/300',
     'Jean Randonneur'
 ),
